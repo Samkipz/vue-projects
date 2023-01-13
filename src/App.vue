37b45c0d20c8dd7032f6381bb687f750df756d71
samkipp <sndiema@cytonn.com>
2023-01-13 13:36:30 +0300

    remade structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
-    <h1> {{ Topic }}</h1>
-    <p>Typed text: {{ text }}</p>
-    <input type="text" ref="name" v-model="text">
-    <div></div>
-    <button @click="handleClick"> buttonClick </button>
-
-    <br>
-    <div v-if="showBooks">
-        <h5><u>Click on a book to hide</u></h5>
-        <ul>
-            <li v-for="(book, index) in showBooks" v-bind:key="index" :class="{ fav: book.isFav }"
-                @click="toggleFav(book)">
-                <h3>{{ book.title }}</h3>
-                <p> {{ book.author }}</p>
-            </li>
-        </ul>
-    </div>
-
-    <button @click="toggleShowBooks">
-        <span v-if="showBooks">Hide All Books</span>
-        <span v-else>Show Books</span>
-    </button>
+    //..
 </template>
 
 <script>
@@ -28,32 +7,17 @@
 export default {
     name: 'App',
     data() {
-        return {
-            Topic: 'My first vue application',
-            text: '',
-            url: 'http://books.toscrape.com/',
-            books: [
-                { title: 'A thousand splendid sunset', author: 'Patrick big', isFav: true },
-                { title: 'Meeting Kwamboka', author: 'Juma Konnan', isFav: true },
-                { title: 'Many ways no way', author: 'Samkipz', isFav: false },
-            ]
-        }
+        //...
     },
     methods: {
-        handleClick() {
-            alert("Button clicked!")
-        },
-        toggleShowBooks() {
-            this.showBooks = !this.showBooks
-        },
-        toggleFav(book) {
-            book.isFav = !book.isFav
-        }
+        //..
     },
     computed: {
-        showBooks() {
-            return this.books.filter((item) => item.isFav)
-        }
+        //..
     }
 }
 </script>
+
+<style lang="scss">
+
+</style>
